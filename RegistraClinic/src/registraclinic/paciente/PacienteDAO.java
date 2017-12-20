0a1edafe676a6eb45d40e1431f4c793229f207e4
Karlos Oliveira <karlos_s.o@hotmail.com>
2017-12-20 07:35:35 -0300

    Adição da classe PacienteDAO
    Classe que herda de GenericDAO e na mesma é adicionado o método salvar

---
+++ RegistraClinic/src/registraclinic/paciente/PacienteDAO.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package registraclinic.paciente;
+
+import javax.swing.JOptionPane;
+import registraclinic.util.GenericDAO;
+
+/**
+ *
+ * @author Karlos
+ */
+public class PacienteDAO extends GenericDAO<Paciente>{
+    
+    public PacienteDAO() {
+        super(Paciente.class);
+    }
+    
+    public void salvar(Paciente paciente) {
+        Object[] options = {"Sim", "Não"};
+        if (paciente.getIdPaciente() == 0) {
+            if (adicionar(paciente)) {
+                JOptionPane.showMessageDialog(null, "Paciente(a) cadastrado(a) com sucesso!");
+            }
+        } else if (JOptionPane.showOptionDialog(null, "Deseja mesmo realizar essa edição"
+                + "?", "RegistraClinic", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]) == JOptionPane.YES_OPTION) {
+            if (atualizar(paciente)) {
+                JOptionPane.showMessageDialog(null, "Paciente(a) editado(a) com sucesso!!");
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "A edição foi cancelada!");
+        }
+    }
+    
+}
