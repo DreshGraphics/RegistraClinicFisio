a194923d843325df91b4ece020bbe233cd4c23f2
Karlos Oliveira <karlos_s.o@hotmail.com>
2017-12-20 07:30:20 -0300

    Classe Paciente
    Inserção dos atributos dos pacientes

--- RegistraClinic/src/registraclinic/paciente/Paciente.java
+++ RegistraClinic/src/registraclinic/paciente/Paciente.java
@@ -13,9 +13,112 @@ public class Paciente {
     
     private int idPaciente;
     private String nomePaciente;
-    private int rgPaciente;
-    private int cpfPaciente;
-    private int idPaciente;
+    private String rgPaciente;
+    private String cpfPaciente;
+    private String dataNascimentoPaciente;
+    private String enderecoPaciente;
+    private String telefonePaciente;
+    private String nomeResponsavelPaciente;
+    private String telefoneResponsavelPaciente;
+    private String dataCadastroPaciente;
+    private boolean urgenciaPaciente;
+    private int numeroProntuarioPaciente;
+
+    public int getIdPaciente() {
+        return idPaciente;
+    }
+
+    public void setIdPaciente(int idPaciente) {
+        this.idPaciente = idPaciente;
+    }
+
+    public String getNomePaciente() {
+        return nomePaciente;
+    }
+
+    public void setNomePaciente(String nomePaciente) {
+        this.nomePaciente = nomePaciente;
+    }
+
+    public String getRgPaciente() {
+        return rgPaciente;
+    }
+
+    public void setRgPaciente(String rgPaciente) {
+        this.rgPaciente = rgPaciente;
+    }
+
+    public String getCpfPaciente() {
+        return cpfPaciente;
+    }
+
+    public void setCpfPaciente(String cpfPaciente) {
+        this.cpfPaciente = cpfPaciente;
+    }
+
+    public String getDataNascimentoPaciente() {
+        return dataNascimentoPaciente;
+    }
+
+    public void setDataNascimentoPaciente(String dataNascimentoPaciente) {
+        this.dataNascimentoPaciente = dataNascimentoPaciente;
+    }
+
+    public String getEnderecoPaciente() {
+        return enderecoPaciente;
+    }
+
+    public void setEnderecoPaciente(String enderecoPaciente) {
+        this.enderecoPaciente = enderecoPaciente;
+    }
+
+    public String getTelefonePaciente() {
+        return telefonePaciente;
+    }
+
+    public void setTelefonePaciente(String telefonePaciente) {
+        this.telefonePaciente = telefonePaciente;
+    }
+
+    public String getNomeResponsavelPaciente() {
+        return nomeResponsavelPaciente;
+    }
+
+    public void setNomeResponsavelPaciente(String nomeResponsavelPaciente) {
+        this.nomeResponsavelPaciente = nomeResponsavelPaciente;
+    }
+
+    public String getTelefoneResponsavelPaciente() {
+        return telefoneResponsavelPaciente;
+    }
+
+    public void setTelefoneResponsavelPaciente(String telefoneResponsavelPaciente) {
+        this.telefoneResponsavelPaciente = telefoneResponsavelPaciente;
+    }
+
+    public String getDataCadastroPaciente() {
+        return dataCadastroPaciente;
+    }
+
+    public void setDataCadastroPaciente(String dataCadastroPaciente) {
+        this.dataCadastroPaciente = dataCadastroPaciente;
+    }
+
+    public boolean isUrgenciaPaciente() {
+        return urgenciaPaciente;
+    }
+
+    public void setUrgenciaPaciente(boolean urgenciaPaciente) {
+        this.urgenciaPaciente = urgenciaPaciente;
+    }
+
+    public int getNumeroProntuarioPaciente() {
+        return numeroProntuarioPaciente;
+    }
+
+    public void setNumeroProntuarioPaciente(int numeroProntuarioPaciente) {
+        this.numeroProntuarioPaciente = numeroProntuarioPaciente;
+    }
     
     
 }
--- RegistraClinic/src/registraclinic/util/HibernateUtil.java
+++ RegistraClinic/src/registraclinic/util/HibernateUtil.java
@@ -12,7 +12,7 @@ import org.hibernate.SessionFactory;
  * Hibernate Utility class with a convenient method to get Session Factory
  * object.
  *
- * @author Katyeudo
+ * @author Karlos
  */
 public class HibernateUtil {
 
