1e83d0359fbfe3e9d57cae8bee0b32ad598c5aac
Karlos Oliveira <karlos.1914.so@gmail.com>
2018-05-12 11:12:48 -0300

    Tab Triagem color

--- RegistraClinicFisioterapia/src/registraclinic/triagem/TriagemCellRenderer.java
+++ RegistraClinicFisioterapia/src/registraclinic/triagem/TriagemCellRenderer.java
@@ -58,60 +58,107 @@ public class TriagemCellRenderer extends DefaultTableCellRenderer {
 //                label.setBackground(Color.YELLOW);
 //            }
 //        }
-//        //obter o objeto da linhas correspondente
-//        //Triagem triagem = ((TriagemTableModel) table.getModel()).getValoresTriagem(row);
-//        //e fazer os if's direto nos valores do objeto, nesse caso para colorir a linha inteira - Só que não sei kkkk
 //        return label;
 //    }
+    
     //Letras coloridas
+//    @Override
+//    public Component getTableCellRendererComponent(JTable table,
+//            Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+//        JLabel label = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus,
+//                row, column);
+//        if (isSelected) {
+//            label.setForeground(Color.WHITE);
+//            label.setBackground(Color.GRAY);
+//            switch (value.toString()) {
+//                case "Verde":
+//                    label.setForeground(Color.GREEN);
+//                    break;
+//                case "Azul":
+//                    label.setForeground(new Color(30, 144, 255));
+//                    break;
+//                case "Laranja":
+//                    label.setForeground(Color.ORANGE);
+//                    break;
+//                case "Amarelo":
+//                    label.setForeground(Color.YELLOW);
+//                    break;
+//                default:
+//                    break;
+//            }
+//        } else {
+//            label.setForeground(Color.BLACK);
+//            label.setBackground(new Color(245,245,245));
+//            switch (value.toString()) {
+//                case "Verde":
+//                    label.setForeground(Color.GREEN);
+//                    break;
+//                case "Azul":
+//                    label.setForeground(new Color(30, 144, 255));
+//                    break;
+//                case "Laranja":
+//                    label.setForeground(Color.ORANGE);
+//                    break;
+//                case "Amarelo":
+//                    label.setForeground(Color.YELLOW);
+//                    break;
+//                default:
+//                    break;
+//            }
+//        }
+//        return label;
+//    }
+    
+    //Linha com fundo colorido
+//    @Override
+//    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+//        Component comp = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+//
+//        String estado = table.getModel().getValueAt(row, 3).toString();
+//        
+//        switch (estado) {
+//            case "Azul":
+//                comp.setBackground(new Color(30, 144, 255));
+//                break;
+//            case "Verde":
+//                comp.setBackground(Color.GREEN);
+//                break;
+//            case "Laranja":
+//                comp.setBackground(Color.ORANGE);
+//                break;
+//            case "Amarelo":
+//                comp.setBackground(Color.YELLOW);
+//                break;
+//            default:
+//                break;
+//        }
+//        return comp;
+//    }
+    
+    //Linha com letras coloridas
     @Override
-    public Component getTableCellRendererComponent(JTable table,
-            Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-        JLabel label = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus,
-                row, column);
-        if (isSelected) {
-            label.setForeground(Color.WHITE);
-            label.setBackground(Color.GRAY);
-            switch (value.toString()) {
-                case "Verde":
-                    label.setForeground(Color.GREEN);
-                    break;
-                case "Azul":
-                    label.setForeground(new Color(30, 144, 255));
-                    break;
-                case "Laranja":
-                    label.setForeground(Color.ORANGE);
-                    break;
-                case "Amarelo":
-                    label.setForeground(Color.YELLOW);
-                    break;
-                default:
-                    break;
-            }
-        } else {
-            label.setForeground(Color.BLACK);
-            label.setBackground(new Color(245,245,245));
-            switch (value.toString()) {
-                case "Verde":
-                    label.setForeground(Color.GREEN);
-                    break;
-                case "Azul":
-                    label.setForeground(new Color(30, 144, 255));
-                    break;
-                case "Laranja":
-                    label.setForeground(Color.ORANGE);
-                    break;
-                case "Amarelo":
-                    label.setForeground(Color.YELLOW);
-                    break;
-                default:
-                    break;
-            }
-        }
-        //obter o objeto da linhas correspondente
-        //Triagem triagem = ((TriagemTableModel)table.getModel()).getValoresTriagem(row);
-        //e fazer os if's direto nos valores do objeto, nesse caso para colorir a linha inteira - Só que não sei kkkk
-        return label;
-    }
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        Component comp = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
 
+        String estado = table.getModel().getValueAt(row, 3).toString();
+        
+        switch (estado) {
+            case "Azul":
+                comp.setForeground(new Color(30, 144, 255));
+                break;
+            case "Verde":
+                comp.setForeground(Color.GREEN);
+                break;
+            case "Laranja":
+                comp.setForeground(Color.ORANGE);
+                break;
+            case "Amarelo":
+                comp.setForeground(Color.YELLOW);
+                break;
+            default:
+                break;
+        }
+        return comp;
+    }
+    
 }
